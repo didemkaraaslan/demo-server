c60fdf5e8bf6dee71815d2a96b32717ca3a2d4d6
Ashley20 <kucukkaraaslan.didem@gmail.com>
2019-08-07 15:05:26 +0300

    Allow browser access to server :)

--- Constants.js
+++ Constants.js
@@ -1,9 +1,12 @@
-const QUERY_SELECT_EVERYTHING_FROM_PRODUCTS = 'SELECT * FROM products';
-const QUERY_SELECT_EVERYTHING_FROM_CATEGORIES = 'SELECT * FROM categories';
-const QUERY_INSERT_INTO_PRODUCTS_TABLE = 'INSERT INTO products SET ?';
-const QUERY_INSERT_INTO_CATEGORIES_TABLE = 'INSERT INTO categories SET ?';
-const QUERY_DELETE_FROM_PRODUCTS_TABLE = 'DELETE FROM products WHERE productId =';
-const QUERY_DELETE_FROM_CATEGORIES_TABLE = 'DELETE FROM categories WHERE categoryId =';
+const QUERY_SELECT_EVERYTHING_FROM_PRODUCTS = "SELECT * FROM products";
+const QUERY_SELECT_EVERYTHING_FROM_CATEGORIES = "SELECT * FROM categories";
+const QUERY_INSERT_INTO_PRODUCTS_TABLE = "INSERT INTO products SET ?";
+const QUERY_INSERT_INTO_CATEGORIES_TABLE = "INSERT INTO categories SET ?";
+const QUERY_DELETE_FROM_PRODUCTS_TABLE =
+  "DELETE FROM products WHERE productId =";
+const QUERY_DELETE_FROM_CATEGORIES_TABLE =
+  "DELETE FROM categories WHERE categoryId =";
+ const QUERY_UPDATE_PRODUCT = "UPDATE products SET productName = ?, productImage = ?,  productPrice = ?,  productStockQuantity = ?,  productCategoryName = ? WHERE productId = ?";
 
 const QUERY_CREATE_PRODUCTS_TABLE = `create table if not exists products(
                         productId int primary key auto_increment,
@@ -19,20 +22,20 @@ const QUERY_CREATE_CATEGORIES_TABLE = `create table if not exists categories(
                         categoryName varchar(255) not null
                     )`;
 const mysqlAuth = {
-  host: 'localhost',
-  user: 'root',
-  password: 'bumpintoyoU1_',
-  database: 'productsDb'
+  host: "localhost",
+  user: "root",
+  password: "bumpintoyoU1_",
+  database: "productsDb"
 };
 
-
 // Socket.io events
-const EVENT_FETCH_ALL_PRODUCTS = 'products';
-const EVENT_FETCH_ALL_CATEGORIES = 'categories';
-const EVENT_ADD_NEW_PRODUCT = 'add product';
-const EVENT_ADD_NEW_CATEGORY = 'add category';
-const EVENT_DELETE_PRODUCT = 'delete product';
-const EVENT_DELETE_CATEGORY = 'delete category';
+const EVENT_FETCH_ALL_PRODUCTS = "products";
+const EVENT_FETCH_ALL_CATEGORIES = "categories";
+const EVENT_ADD_NEW_PRODUCT = "add product";
+const EVENT_ADD_NEW_CATEGORY = "add category";
+const EVENT_DELETE_PRODUCT = "delete product";
+const EVENT_DELETE_CATEGORY = "delete category";
+const EVENT_UPDATE_PRODUCT = "update product";
 
 module.exports = {
   QUERY_SELECT_EVERYTHING_FROM_PRODUCTS,
@@ -41,6 +44,7 @@ module.exports = {
   QUERY_INSERT_INTO_CATEGORIES_TABLE,
   QUERY_DELETE_FROM_PRODUCTS_TABLE,
   QUERY_DELETE_FROM_CATEGORIES_TABLE,
+  QUERY_UPDATE_PRODUCT,
   QUERY_CREATE_PRODUCTS_TABLE,
   QUERY_CREATE_CATEGORIES_TABLE,
   mysqlAuth,
@@ -49,5 +53,6 @@ module.exports = {
   EVENT_ADD_NEW_PRODUCT,
   EVENT_ADD_NEW_CATEGORY,
   EVENT_DELETE_PRODUCT,
-  EVENT_DELETE_CATEGORY
+  EVENT_DELETE_CATEGORY,
+  EVENT_UPDATE_PRODUCT
 };
--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const {
   QUERY_INSERT_INTO_CATEGORIES_TABLE,
   QUERY_DELETE_FROM_PRODUCTS_TABLE,
   QUERY_DELETE_FROM_CATEGORIES_TABLE,
+  QUERY_UPDATE_PRODUCT,
   QUERY_CREATE_PRODUCTS_TABLE,
   QUERY_CREATE_CATEGORIES_TABLE,
   mysqlAuth,
@@ -20,38 +21,43 @@ const {
   EVENT_ADD_NEW_PRODUCT,
   EVENT_ADD_NEW_CATEGORY,
   EVENT_DELETE_PRODUCT,
-  EVENT_DELETE_CATEGORY
+  EVENT_DELETE_CATEGORY,
+  EVENT_UPDATE_PRODUCT
 } = require("./Constants.js");
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-const whitelist = ['http://localhost:3001', 'http://www.apirequest.io']
+const whitelist = ["http://localhost:3001", "http://www.apirequest.io"];
 const corsOptions = {
   credentials: true,
-   origin: function (origin, callback) {
-     if (whitelist.indexOf(origin) !== -1) {
-       callback(null, true)
-     } else {
-       callback(new Error(`${origin} not allowed by CORS`))
-     }
-   }
-}
+  origin: function(origin, callback) {
+    if(origin == null) {
+      callback(null, true);
+    } else if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error(`${origin} not allowed by CORS`));
+    }
+  }
+};
 app.use(cors(corsOptions));
-app.use(bodyParser.json())
+
+// app.options('*', cors())
+app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
 // Create socket client
 const io = socketIO(server);
 
-io.on('connection', socket => {
-  console.log('User connected')
+io.on("connection", socket => {
+  console.log("User connected");
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected')
-  })
-})
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
 // Create MySQL connection
 const mysqlConnection = mysql.createConnection({ ...mysqlAuth });
@@ -59,53 +65,67 @@ const mysqlConnection = mysql.createConnection({ ...mysqlAuth });
 // Connect to MySQL database
 mysqlConnection.connect(err => {
   if (err) {
-    console.error('error connecting: ' + err.stack);
+    console.error("error connecting: " + err.stack);
     return;
   }
 
-  mysqlConnection.query(QUERY_CREATE_PRODUCTS_TABLE, function(err, results, fields) {
+  mysqlConnection.query(QUERY_CREATE_PRODUCTS_TABLE, function(
+    err,
+    results,
+    fields
+  ) {
     if (err) {
       console.log(err.message);
     }
   });
 
-  mysqlConnection.query(QUERY_CREATE_CATEGORIES_TABLE, function(err, results, fields) {
+  mysqlConnection.query(QUERY_CREATE_CATEGORIES_TABLE, function(
+    err,
+    results,
+    fields
+  ) {
     if (err) {
       console.log(err.message);
     }
   });
 
-/* Product Management Methods */
+  /* Product Management Methods */
   app.get("/api/products", (req, res) => {
     // Query the database for all the products
-    mysqlConnection.query(QUERY_SELECT_EVERYTHING_FROM_PRODUCTS, (err, rows, fields) => {
-      if(err) {
-        console.error(err);
-        return res.json({
-          message: `Failure! We couldn't bring products list.`
-        });
-      } else {
-        io.emit(EVENT_FETCH_ALL_PRODUCTS, rows);
-        return res.json(rows);
+    mysqlConnection.query(
+      QUERY_SELECT_EVERYTHING_FROM_PRODUCTS,
+      (err, rows, fields) => {
+        if (err) {
+          console.error(err);
+          return res.json({
+            message: `Failure! We couldn't bring products list.`
+          });
+        } else {
+          io.emit(EVENT_FETCH_ALL_PRODUCTS, rows);
+          return res.json(rows);
+        }
       }
-    })
+    );
   });
 
   app.post("/api/productManagement", (req, res) => {
-    const product = {...req.body};
+    const product = { ...req.body };
 
-    mysqlConnection.query(QUERY_INSERT_INTO_PRODUCTS_TABLE, product, function(err, result) {
-      if(err) {
+    mysqlConnection.query(QUERY_INSERT_INTO_PRODUCTS_TABLE, product, function(
+      err,
+      result
+    ) {
+      if (err) {
         console.error(err);
         res.json({
           message: `Failure! Product ${product.productName} couldnt be saved.`
         });
       } else {
-          io.emit(EVENT_ADD_NEW_PRODUCT, product);
-          res.json({
-            status: 200,
-            message: `Success! Product ${product.productName} with the insertId of ${result.insertId} is saved.`
-          });
+        io.emit(EVENT_ADD_NEW_PRODUCT, product);
+        res.json({
+          status: 200,
+          message: `Success! Product ${product.productName} with the insertId of ${result.insertId} is saved.`
+        });
       }
     });
   });
@@ -113,89 +133,123 @@ mysqlConnection.connect(err => {
   app.delete("/api/productManagement", (req, res) => {
     const productId = req.body.productId;
 
-    mysqlConnection.query(QUERY_DELETE_FROM_PRODUCTS_TABLE + `${productId}`, function(err, result) {
-      if(err) {
-        console.error(err);
-        res.json({
-          message: `Failure! Product ${product.productName}  couldn't be deleted.`
-        });
-      } else {
+    mysqlConnection.query(
+      QUERY_DELETE_FROM_PRODUCTS_TABLE + `${productId}`,
+      function(err, result) {
+        if (err) {
+          console.error(err);
+          res.json({
+            message: `Failure! Product with the id of ${productId}  couldn't be deleted.`
+          });
+        } else {
           io.emit(EVENT_DELETE_PRODUCT, productId);
           res.json({
             status: 200,
-            message: `Success! Product ${product.productName} with the insertId of ${result.insertId} is deleted.`
+            message: `Success! Product with the insertId of ${result.insertId} is deleted.`
           });
+        }
+      }
+    );
+  });
+
+  // Update product here
+  app.put("/api/productManagement", (req, res) => {
+    const product = { ...req.body };
+
+    mysqlConnection.query(QUERY_UPDATE_PRODUCT,
+     [product.productName, product.productImage, product.productPrice, product.productStockQuantity, product.productCategoryName, product.productId], function(
+      err,
+      result
+    ) {
+      if (err) {
+        console.error(err);
+        res.json({
+          message: `Failure! Product with the id of ${product.productId} couldnt be updated.`
+        });
+      } else {
+        io.emit(EVENT_UPDATE_PRODUCT, product);
+        res.json({
+          status: 200,
+          message: `Success! Product with the id of ${product.productId} is updated`
+        });
       }
     });
-
   });
 
+  /* Category Management Methods */
 
-/* Category Management Methods */
-
-app.get("/api/categories", (req, res) => {
-  // Query the database for all the categories
-  mysqlConnection.query(QUERY_SELECT_EVERYTHING_FROM_CATEGORIES, (err, rows, fields) => {
-    if(err) {
-      console.error(err);
-      res.json({
-        message: `Failure! We couldn't bring categories list.`
-      });
-    } else {
-      io.emit(EVENT_FETCH_ALL_CATEGORIES, rows);
-      res.json(rows);
-    }
-  })
-});
-
-app.post("/api/categoryManagement", (req, res) => {
-  const category = {...req.body};
-
-  mysqlConnection.query(QUERY_INSERT_INTO_CATEGORIES_TABLE, category, function(err, result) {
-    if(err) {
-      console.error(err);
-      res.json({
-        message: `Failure! Category ${category.categoryName}  couldn't be saved.`
-      });
-    } else {
-        io.emit(EVENT_ADD_NEW_CATEGORY, { categoryId: result.insertId, categoryName: category.categoryName });
-        res.json({
-          status: 200,
-          message: `Success! Category ${category.categoryName} with the insertId of ${result.insertId} is saved.`
-        });
-    }
-  });
-});
-
-app.delete("/api/categoryManagement", (req, res) => {
-  const categoryId = req.body.categoryId;
-
-  mysqlConnection.query(QUERY_DELETE_FROM_CATEGORIES_TABLE + `${categoryId}`, function(err, result) {
-    if(err) {
-      console.error(err);
-      res.json({
-        message: `Failure! Category with the id of ${categoryId}  couldn't be deleted.`
-      });
-    } else {
-        io.emit(EVENT_DELETE_CATEGORY, categoryId);
-        res.json({
-          status: 200,
-          message: `Success! Category  with the insertId of ${result.insertId} is deleted.`
-        });
-    }
+  app.get("/api/categories", (req, res) => {
+    // Query the database for all the categories
+    mysqlConnection.query(
+      QUERY_SELECT_EVERYTHING_FROM_CATEGORIES,
+      (err, rows, fields) => {
+        if (err) {
+          console.error(err);
+          res.json({
+            message: `Failure! We couldn't bring categories list.`
+          });
+        } else {
+          io.emit(EVENT_FETCH_ALL_CATEGORIES, rows);
+          res.json(rows);
+        }
+      }
+    );
   });
 
-});
+  app.post("/api/categoryManagement", (req, res) => {
+    const category = { ...req.body };
 
+    mysqlConnection.query(
+      QUERY_INSERT_INTO_CATEGORIES_TABLE,
+      category,
+      function(err, result) {
+        if (err) {
+          console.error(err);
+          res.json({
+            message: `Failure! Category ${category.categoryName}  couldn't be saved.`
+          });
+        } else {
+          io.emit(EVENT_ADD_NEW_CATEGORY, {
+            categoryId: result.insertId,
+            categoryName: category.categoryName
+          });
+          res.json({
+            status: 200,
+            message: `Success! Category ${category.categoryName} with the insertId of ${result.insertId} is saved.`
+          });
+        }
+      }
+    );
+  });
 
-/*  mysqlConnection.end(function(err) {
+  app.delete("/api/categoryManagement", (req, res) => {
+    const categoryId = req.body.categoryId;
+
+    mysqlConnection.query(
+      QUERY_DELETE_FROM_CATEGORIES_TABLE + `${categoryId}`,
+      function(err, result) {
+        if (err) {
+          console.error(err);
+          res.json({
+            message: `Failure! Category with the id of ${categoryId}  couldn't be deleted.`
+          });
+        } else {
+          io.emit(EVENT_DELETE_CATEGORY, categoryId);
+          res.json({
+            status: 200,
+            message: `Success! Category  with the insertId of ${result.insertId} is deleted.`
+          });
+        }
+      }
+    );
+  });
+
+  /*  mysqlConnection.end(function(err) {
     if (err) {
       return console.log(err.message);
     }
   }); */
 });
-
-
 
 server.listen(port, () => {
   console.log(`Listening on port: ${port}`);
